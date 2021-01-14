0b68fdd8d2b57402bfdfcb1dd2e2b28053762cb4
amirhosein <amirhoseinheshmati@gmail.com>
2021-01-14 11:31:20 +0330

    updating env and stuff

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+mkdir "/var/log/dreamer"
+mkdir "/var/log/dreamer/celery"
+mkdir "/var/log/dreamer/postgresql_docker_dreamer"
+mkdir "/etc/postgresql_docker_dreamer"
+
+docker-compose up
