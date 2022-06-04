afc536741b449863b2316e378da8cc98da221667
amirhosein <amirhoseinheshmati@gmail.com>
2022-06-04 12:02:40 +0430

    removed bash

--- src/runner/celery_beat_run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-celery -A dreamer beat --logfile=/var/log/dreamer/celery/beat.log --loglevel=INFO --schedule=/var/run/celery/celerybeat-schedule
--- src/runner/celery_run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-celery -A dreamer worker --loglevel=INFO --logfile=/var/log/dreamer/celery/celery.log --time-limit=300 --autoscale=3,1
